e6fb4e72a94e1e44f1d2f2eaed1a0fcdaaec08b8
Abid Hussain <abid.abid97@gmail.com>
2017-09-06 19:06:40 +0530

    bug fix

--- static/Dashboard.js
+++ static/Dashboard.js
@@ -5,28 +5,47 @@ $(document).ready(function(){
 
 
   var instructionArr = [];
-  $('#addressTxtBox').focus();
-  $('#btnnext').click(function(){
-    var address = $('#addressTxtBox').val()
-    var nextAddr = parseInt(address) + 1
-    var instruction = $('#instructionTxtBox').val()
-    var arrElement = address + " " + instruction;
-    console.log(arrElement)
-    instructionArr.push(arrElement);
-    console.log(instructionArr);
-    $('#addressTxtBox').val(nextAddr);
-    $('#instructionTxtBox').val("");
-    $('#instructionTxtBox').focus();
+  $("#addressTxtBox").attr("disabled", "disabled")
+  $("#instructionTxtBox").attr("disabled", "disabled")
+
+  $("#btnreset").click(function() {
+    $("#addressTxtBox").attr("disabled", "disabled")
+    $("#instructionTxtBox").attr("disabled", "disabled")
   })
 
-  $('#btnreset').click(function(){
+  //on click function of memory Button
+  $("#btnmem").click(function() {
+    $("#addressTxtBox").removeAttr("disabled")
+    $("#instructionTxtBox").removeAttr("disabled")
+    $('#addressTxtBox').focus();
+  })
+
+  //on click function of next Button
+  $('#btnnext').click(function() {
+    var address = $('#addressTxtBox').val()
+    var instruction = $('#instructionTxtBox').val()
+    if (address == "" && instruction == "") {
+      console.log("Inputs are Empty");
+    } else {
+      //inserting elements into the array and incrementing the address value
+      var nextAddr = parseInt(address) + 1
+      var arrElement = address + " " + instruction;
+      console.log(arrElement)
+      instructionArr.push(arrElement);
+      console.log(instructionArr);
+      $('#addressTxtBox').val(nextAddr);
+      $('#instructionTxtBox').val("");
+      $('#instructionTxtBox').focus();
+    }
+  })
+
+  $('#btnexec').click(function() {
     $.ajax({
       type: 'POST',
-      url: '',
-      data: {instructionArr:instructionArr},
+      url: 'execute?ins_arr='+instructionArr,
       datatype:'json',
       encode: true
-    }).done(function(){
+    }).done(function() {
       console.log("done");
     })
   })
